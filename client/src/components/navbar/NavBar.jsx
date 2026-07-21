50a5dbf6017e906ad10e1ada520207dfcb38a06b
Nitish Minz <nitishkumar083@gmail.com>
2026-07-21 15:16:00 +0530

    responsive for small screen for few section is added

--- client/src/components/navbar/NavBar.jsx
+++ client/src/components/navbar/NavBar.jsx
@@ -6,6 +6,7 @@ import {Link, useNavigate} from 'react-router-dom'
 function NavBar() {
   const [isLogin,setIslogin] = useState(false)
   const [userName,setUserName] = useState("")
+  const [togglemenu, setToggleMenu] = useState(false)
   
     const navigate = useNavigate()
     const data = Object.entries( useSelector(state=>state.products.cart))
@@ -45,22 +46,45 @@ const goToHome=()=>{
   console.log("navbar")
   return (
     <div id="navContainer">
-      { console.log(userName)}
+      {/* { console.log(userName)} */}
           <div id="appIcon" onClick={goToHome}>
+            <div style={{display:"flex",gap:"20px",width:"250px",alignItems:"center"}}>
             <img src='https://media.istockphoto.com/id/1029895828/vector/shopping-bag-with-cart-logo-design-illustrator.jpg?s=612x612&w=0&k=20&c=HE8fwTY9FmqkEMY9qI-NFeQEo-g0cxE5xu6_fvZZrY0=' alt=""/>
             <h1 onClick={goToHome}>SHOPPER CART</h1>
+            </div>
+              <div className='hamburgerMenu' onClick={()=>setToggleMenu(!togglemenu)}>
+            <div></div>
+             <div></div>
+            <div></div>
+
+
           </div>
-          <div id="search">
+          </div>
+         <div   id="search">
             <input type="search" placeholder='search'/>
             <div></div>
           </div>
-          <div id="userDetails">
+          <div style={togglemenu? {display:"block"}:{display:"none"}}  id="search">
+            <input type="search" placeholder='search'/>
+            <div></div>
+          </div>
+          <div  id="userDetails">
             
             {isLogin && isLogin? <div style={{display:"flex"}}><p>{userName}</p><button onClick={logoutAccout}>Logout</button></div>:<button onClick={loginAccout}>Login</button>}
           </div>
-          <h3 ><span style={{color:"red"}}>&#x2606;</span> <Link to='https://shoppercart-u4od.vercel.app'style={{textDecoration:"none",color:'black'}}>Become a Seller</Link></h3>
+         <div style={togglemenu? {display:"flex"}:{display:"none"}} id="userDetails">
+            
+            {isLogin && isLogin? <div style={{display:"flex"}}><p>{userName}</p><button onClick={logoutAccout}>Logout</button></div>:<button onClick={loginAccout}>Login</button>}
+          </div>
 
-          <div id="cartIcon" onClick={goToCart}>
+          <h3 style={togglemenu? {display:"flex"}:{display:"none"}} className="linkTosellerWebsite"><span style={{color:"red"}}>&#x2606;</span> <Link to='https://shoppercart-u4od.vercel.app'style={{textDecoration:"none",color:'black'}}>Become a Seller</Link></h3>
+          <h3  className="linkTosellerWebsite"><span style={{color:"red"}}>&#x2606;</span> <Link to='https://shoppercart-u4od.vercel.app'style={{textDecoration:"none",color:'black'}}>Become a Seller</Link></h3>
+
+          <div style={togglemenu? {display:"flex"}:{display:"none"}} id="cartIcon" onClick={goToCart}>
+          <i className="fa fa-shopping-cart"></i>
+          <span style={{    fontFamily: 'Trebuchet MS, Lucida Sans Unicode,Lucida Grande, Lucida Sans, Arial, sans-serif'}}>{data.length}</span>
+          </div>
+          <div  id="cartIcon" onClick={goToCart}>
           <i className="fa fa-shopping-cart"></i>
           <span style={{    fontFamily: 'Trebuchet MS, Lucida Sans Unicode,Lucida Grande, Lucida Sans, Arial, sans-serif'}}>{data.length}</span>
           </div>
